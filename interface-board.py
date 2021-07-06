f748a7d0becad3f87a919b19fb34c47f866345ce
Jeroval <jerome.valens@gmail.com>
2021-07-06 16:54:35 +0200

    Création de la fenêtre d'interface

---
+++ interface-board.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# Creation de premiere fenetre
+windows = Tk()
+
+# personalisation de la fenetre
+windows.title ("Coleoptera")
+windows.geometry("480x360")
+windows.minsize (480,360)
+
+# afficher
+windows.mainloop()
