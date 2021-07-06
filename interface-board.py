6e95c9a1fce735b7c4b176b86eb1ba1fd76c358c
Jeroval <jerome.valens@gmail.com>
2021-07-07 00:15:21 +0200

    Travail sur interface

--- interface-board.py
+++ interface-board.py
@@ -8,18 +8,18 @@ def open_github():
 
 
 # Creation de premiere fenetre laisser au début
-window = Tk()
+root = Tk()
 
 
 # personalisation de la fenetre
-window.title ("Coleoptera")
-window.geometry("1280x720")
-window.minsize (480,360)
-window.iconbitmap("logo-coleoptera.ico")
-window.config(background='#0B008D')
+root.title ("Coleoptera")
+root.geometry("1280x720")
+root.minsize (480,360)
+root.iconbitmap("logo-coleoptera.ico")
+root.config(background='#292929')
 
 # ajout du texte
-label_title = Label(window, text="Coleoptera s'occupe des fichiers inutiles pour vous !", font=("Calibri", 10), bg='purple', fg='white', bd=1, relief=SUNKEN)
+label_title = Label(root, text="Coleoptera s'occupe des fichiers inutiles pour vous ! Version Alpha test", font=("Calibri", 10), bg='purple', fg='white', bd=1, relief=SUNKEN)
 label_title.pack(side=BOTTOM)
 
 # Bouton A - Parametres
@@ -29,15 +29,21 @@ bouton_a_git.pack(side=LEFT)
 bouton_a_git = Button (text = "Logs", font=("Calibri", 20), bg='#0C5A00', fg='white', bd=1, relief=SUNKEN)
 bouton_a_git.pack(side=LEFT)
 # Bouton C - Actions
-bouton_a_git = Button (text = "Actions", font=("Calibri", 20), bg='#0C5A00', fg='white', bd=1, relief=SUNKEN)
+bouton_a_git = Button (text = "Recommandations", font=("Calibri", 20), bg='#0C5A00', fg='white', bd=1, relief=SUNKEN)
 bouton_a_git.pack(side=LEFT)
-# Bouton E - Git
+# Bouton D - Lancer le scan
+bouton_a_git = Button (text = "Démarrer l'analyse", font=("Calibri", 20), bg='#0C5A00', fg='white', bd=1, relief=SUNKEN)
+bouton_a_git.pack(side=RIGHT)
+# Bouton E - Stopper le scan
+bouton_a_git = Button (text = "Stoper l'analyse", font=("Calibri", 20), bg='#0C5A00', fg='white', bd=1, relief=SUNKEN)
+bouton_a_git.pack(side=RIGHT)
+# Bouton F - Git
 bouton_a_git = Button (text = "A propos", font=("Calibri", 20), bg='#0C5A00', fg='white', bd=1, relief=SUNKEN, command=open_github)
 bouton_a_git.pack(side=LEFT)
 
 
 # afficher la page laisser à la fin
-window.mainloop()
+root.mainloop()
 
 
 
