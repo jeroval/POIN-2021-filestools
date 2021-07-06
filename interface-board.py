16e1bc619b801c38aa40b3d3c867d9df6dec5236
Jeroval <jerome.valens@gmail.com>
2021-07-06 20:58:48 +0200

    Création de la fenêtre d'interface 3

--- interface-board.py
+++ interface-board.py
@@ -11,9 +11,14 @@ window.iconbitmap("logo-coleoptera.ico")
 window.config(background='#B0DEFF')
 
 # ajout du texte
-label_title = Label(window, text="Coleoptera voit les fichiers inutles et vous averti", font=("Calibri", 20), bg='#B0DEFF', fg='white')
+
+label_title = Label(window, text="Coleoptera s'occupe des fichiers inutiles pour vous", font=("Calibri", 17), bg='#B0DEFF', fg='white', bd=1, relief=SUNKEN)
 label_title.grid(row=0,column=0)
 
+# Bouton A - menu
 
-# afficher la fenetre
+bouton_a_menu = Button (text = "zone de texte", font=("Calibri", 12), bg='#B0DEFF', fg='white', bd=1, relief=SUNKEN)
+bouton_a_menu.pack()
+
+# afficher la entre
 window.mainloop()
