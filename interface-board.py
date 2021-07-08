a8c0682ca8353a3a699c591c8c01d8de2f17b1ee
Jeroval <jerome.valens@gmail.com>
2021-07-08 23:59:03 +0200

    interface

--- interface-board.py
+++ interface-board.py
@@ -21,37 +21,51 @@ root.minsize (480,360)
 root.iconbitmap("logo-coleoptera.ico")
 root.config(background='#292929')
 
+#Creation de la boite à logo
+framea = Frame(root, bg='black')
+
 # ajout du texte titre
-label_title = Label(root, text="Coleoptera s'occupe des fichiers inutiles pour vous ! Version Alpha test", font=("Calibri", 14), bg='black', fg='white', bd=1)
-label_title.pack(side=TOP)
+label_title = Label(framea, text="Coleoptera s'occupe des fichiers inutiles pour vous ! Version Alpha test", font=("Calibri", 14), bg='black', fg='white', bd=1)
+label_title.pack()
+
+# Ajout du logo principal sur la fenetre
+width = 100
+height = 100
+imagea = PhotoImage(file="coleoptera.png").zoom(1).subsample(1)
+canvas = Canvas(framea, width=width, height=height, bg='#292929')
+canvas.create_image(width/2, height/2, image=imagea)
+canvas.pack()
+
+#Fermeture de la boite à logo
+framea.pack(side=TOP)
 
 #Creation de la boite à boutons
-frame = Frame(root, bg='black')
+frameb = Frame(root, bg='black')
 
 # Bouton A - Parametres
-bouton_a_git = Button (frame, text = "Paramètres", font=("Calibri", 14), bg='#010B8B', fg='white', bd=1, relief=SUNKEN)
+bouton_a_git = Button (frameb, text ="Vos réglages ici", font=("Calibri", 14), bg='#010B8B', fg='white', bd=1, relief=SUNKEN)
 bouton_a_git.pack()
 # Bouton B - Logs
-bouton_b_git = Button (frame, text = "Logs", font=("Calibri", 14), bg='#010B8B', fg='white', bd=1, relief=SUNKEN)
+bouton_b_git = Button (frameb, text ="Suivi des actions", font=("Calibri", 14), bg='#010B8B', fg='white', bd=1, relief=SUNKEN)
 bouton_b_git.pack()
 # Bouton C - Actions
-bouton_c_git = Button (frame, text = "Recommandations", font=("Calibri", 14), bg='#010B8B', fg='white', bd=1, relief=SUNKEN)
+bouton_c_git = Button (frameb, text ="Recommandations", font=("Calibri", 14), bg='#010B8B', fg='white', bd=1, relief=SUNKEN)
 bouton_c_git.pack()
 # Bouton D - Lancer le scan
-bouton_d_git = Button (frame, text = "Démarrer l'analyse", font=("Calibri", 14), bg='#010B8B', fg='white', bd=1, relief=SUNKEN)
+bouton_d_git = Button (frameb, text ="Démarrer l'analyse", font=("Calibri", 14), bg='#010B8B', fg='white', bd=1, relief=SUNKEN)
 bouton_d_git.pack()
 # Bouton E - Stopper le scan
-bouton_e_git = Button (frame, text = "Stoper l'analyse", font=("Calibri", 14), bg='#010B8B', fg='white', bd=1, relief=SUNKEN)
+bouton_e_git = Button (frameb, text ="Stoper l'analyse", font=("Calibri", 14), bg='#010B8B', fg='white', bd=1, relief=SUNKEN)
 bouton_e_git.pack()
 # Bouton F - Git
-bouton_f_git = Button (frame, text = "A propos", font=("Calibri", 14), bg='#010B8B', fg='white', bd=1, relief=SUNKEN, command=open_github)
+bouton_f_git = Button (frameb, text ="A propos du logiciel", font=("Calibri", 14), bg='#010B8B', fg='white', bd=1, relief=SUNKEN, command=open_github)
 bouton_f_git.pack()
 # Bouton G - Dons
-bouton_g_git = Button (frame, text = "Soutenez-nous ici", font=("Calibri", 14), bg='#010B8B', fg='white', bd=1, relief=SUNKEN, command=open_dons)
+bouton_g_git = Button (frameb, text ="Soutenez-nous ici", font=("Calibri", 14), bg='#010B8B', fg='white', bd=1, relief=SUNKEN, command=open_dons)
 bouton_g_git.pack()
 
 #Fermeture de la boite à boutons
-frame.pack(side=LEFT)
+frameb.pack(side=LEFT)
 
 
 # afficher la page laisser à la fin
